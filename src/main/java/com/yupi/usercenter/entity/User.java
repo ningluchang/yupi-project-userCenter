9dbe6fba1403d4b786e0d3316322324fd8a4102b
ningluchang <ningluchang17@gmail>
2025-07-05 10:50:51 +0800

    first commit

---
+++ src/main/java/com/yupi/usercenter/entity/User.java
@@ -0,0 +1,20 @@
+package com.yupi.usercenter.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName("user")
+public class User {
+    @TableId(type = IdType.AUTO)
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+}
